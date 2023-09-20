8b3114ad12576466a43d02630894951c5a9fb03f
Muhammad Yahaya <101328329+Muhammad235@users.noreply.github.com>
2023-09-20 22:41:33 +0100

    [create] migrations and controllers

---
+++ database/migrations/2023_09_20_113900_create_admins_table.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->string('username');
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admins');
+    }
+};
+
+
+
+//adim(fixed)
+//username
+//password
+
+
+//admin create student/teacher
+
+//student (oneToOne);
+
+//first name
+//last name
+//student address
+//gender
+//email
+//class
+//password
+//profile image
+
+
+//teacher
+//first name
+//last name
+//email
+//password
+//class - grade(1, 2, 3, 4, 5, 6)
+
+
+
+
+
