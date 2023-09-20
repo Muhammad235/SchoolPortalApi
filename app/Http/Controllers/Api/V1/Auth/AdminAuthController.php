73d882b3a02532c0980cbfa5e6d9da2b30e99512
Muhammad Yahaya <101328329+Muhammad235@users.noreply.github.com>
2023-09-20 23:29:57 +0100

    [create] route

--- app/Http/Controllers/Api/V1/Auth/AdminAuthController.php
+++ app/Http/Controllers/Api/V1/Auth/AdminAuthController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class AdminAuthController extends Controller
 {
-    //
+    public function login()
+    {
+        return "admin auth";
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\V1\Admin\StudentController;
+use App\Http\Controllers\Api\V1\Admin\TeacherController;
+
+use App\Http\Controllers\APi\V1\Auth\AdminAuthController;
+use App\Http\Controllers\APi\V1\Auth\StudentAuthController;
+
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +25,24 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('admin')->group(function (){
+    Route::post('login', [AdminAuthController::class, 'login']);
+
+    // Route::middleware('auth:sanctum')->group(function() {
+        Route::apiResource('/student', StudentController::class);
+        Route::apiResource('/teacher', TeacherController::class);
+    // });
+});
+
+
+Route::prefix('portal')->group(function (){
+    Route::post('register', [StudentAuthController::class, 'register']);
+    Route::post('login', [StudentAuthController::class, 'login']);
+
+    // Route::middleware('auth:sanctum')->group(function() {
+        Route::apiResource('/student', StudentController::class);
+    // });
+});
+
+
